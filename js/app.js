a96eea2c100c4fa28e695aeabeeeac0eef097b5f
carpiee <62463826+carpiee@users.noreply.github.com>
2020-08-22 00:27:04 +0200

    added a menu hiding function when clicking outside the menu

--- js/app.js
+++ js/app.js
@@ -4,19 +4,16 @@ $(document).ready(function () {
     const item = $("#colap_1");
     if (item.hasClass("hidden")) {
       item.removeClass("hidden");
-      item.addClass("block");
     } else {
-      item.removeClass("block");
       item.addClass("hidden");
     }
   });
+
   $("#colap_trigger_2").click(() => {
     const item = $("#colap_2");
     if (item.hasClass("hidden")) {
       item.removeClass("hidden");
-      item.addClass("block");
     } else {
-      item.removeClass("block");
       item.addClass("hidden");
     }
   });
@@ -24,9 +21,7 @@ $(document).ready(function () {
     const item = $("#colap_3");
     if (item.hasClass("hidden")) {
       item.removeClass("hidden");
-      item.addClass("block");
     } else {
-      item.removeClass("block");
       item.addClass("hidden");
     }
   });
@@ -37,9 +32,11 @@ $(document).ready(function () {
   const menu = $("#menu");
   burger.click(function () {
     ShowMenu();
+    console.log("MENU");
   });
 
   function ShowMenu() {
+    // menu.slideToggle("fast");
     if (menu.hasClass("hidden")) {
       menu.removeClass("hidden");
       menu.addClass("flex");
@@ -71,18 +68,27 @@ $(document).ready(function () {
   // dropdown menu
 
   $("#dropdown_container_1").click(function () {
+    $("#dropdown_1").attr("tabIndex", -1);
     if ($("#dropdown_1").hasClass("hidden")) {
       $("#dropdown_1").removeClass("hidden");
     } else {
       $("#dropdown_1").addClass("hidden");
     }
   });
+  $("#dropdown_1").focusout(function () {
+    $("#dropdown_1").addClass("hidden");
+  });
+
   $("#dropdown_container_2").click(function () {
+    $("#dropdown_2").focus();
     if ($("#dropdown_2").hasClass("hidden")) {
       $("#dropdown_2").removeClass("hidden");
     } else {
       $("#dropdown_2").addClass("hidden");
     }
+  });
+  $("#dropdown_2").on("focusout", function (e) {
+    $("#dropdown_2").addClass("hidden");
   });
   $("#dropdown_container_3").click(function () {
     if ($("#dropdown_3").hasClass("hidden")) {
@@ -91,4 +97,23 @@ $(document).ready(function () {
       $("#dropdown_3").addClass("hidden");
     }
   });
+  $("#dropdown_container_3").on("focusout", function (e) {
+    $("#dropdown_3").addClass("hidden");
+  });
+});
+$(document).on("click", function (event) {
+  var trigger = $("#burger");
+  var menu = $("#menu > *");
+  if ($("#menu").hasClass("flex")) {
+    if (
+      trigger !== event.target &&
+      !trigger.has(event.target).length &&
+      menu !== event.target &&
+      !menu.has(event.target).length
+    ) {
+      console.log("close menu");
+      $("#menu").removeClass("flex");
+      $("#menu").addClass("hidden");
+    }
+  }
 });
